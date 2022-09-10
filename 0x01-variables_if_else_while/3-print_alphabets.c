091ae5b640977e852b0efbcad2d5cd0125adbc91
Olugbenga <111020469+bienary@users.noreply.github.com>
2022-09-09 18:49:42 -0700

    print alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print if the number is positive, zero, or negative
+ * main - print if the number is postive, zero, or negative
  *
  * Description: using the main function
  * this program prints "Programming is positive, zero, or negative
@@ -8,15 +8,15 @@
  */
 int main(void)
 {
-	char ch;
-	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{
-		putchar(ch);
-	}
-	for (ch = 'A' ; ch <= 'Z' ; ch++)
-	{
-		putchar(ch);
-	}
-	putchar('\n');
-	return (0);
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
 }
