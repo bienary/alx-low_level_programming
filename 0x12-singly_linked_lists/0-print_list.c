5c280fb07ea97d1b666ae8632a8d36e3c37b02b6
Olugbenga <111020469+bienary@users.noreply.github.com>
2022-10-21 16:18:32 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,5 @@
 #include "lists.h"
-#include <stdio.h>
-
+#include <stdio.h>	
 /**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to the start of the list
@@ -10,8 +9,7 @@
 size_t print_list(const list_t *h)
 {
 	const list_t *tmp;
-	unsigned int i;
-
+	unsigned int i;	
 	tmp = h;
 	for (i = 0; tmp; i++)
 	{
